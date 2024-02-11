71e70734b5ab0e5ac3f4f75b57c6741258b980e7
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2024-02-11 16:40:39 +0200

    Switch to lein
    - remove deps.edn
    - add project.clj
    - change LICENSE to default lein LICENCE
    - move docs/readme.org  readme.org
    - rename docs to doc to follow lein template

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ntheory "0.1.0"
+  :description "Number Theory for clojure"
+  :url "https://github.com/vkocubinsky/ntheory-clojure"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns vk.ntheory})
