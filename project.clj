f6966ba2e88ca2d8e8748ead572311115d2ad311
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2024-02-22 15:59:25 +0200

    Add changelog

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject ntheory "0.1.0"
-  :description "Number Theory for clojure"
+(defproject ntheory "0.2.0"
+  :description "Number Theory with Clojure"
   :url "https://github.com/vkocubinsky/ntheory-clojure"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
