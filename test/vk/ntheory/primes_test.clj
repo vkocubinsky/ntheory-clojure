c8918432f1ebad41751df164e961a839afca1375
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2024-03-18 16:29:01 +0200

    s

--- test/vk/ntheory/primes_test.clj
+++ test/vk/ntheory/primes_test.clj
@@ -1,32 +1,40 @@
 (ns vk.ntheory.primes-test
   (:require
    [clojure.test :refer [deftest is are testing]]
-   [vk.ntheory.primes :as p])
-  )
-
+   [vk.ntheory.primes :as p]))
 
 (deftest primes-test
   (testing "cache"
     (p/cache-reset!)
     (is (= 0 (:upper @p/cache))))
   (testing "small numbers"
-    (is [2] (p/primes 2))
-    (is [2 3] (p/primes 3))
-    (is [2 3] (p/primes 4))
-    (is [2 3 5] (p/primes 5))
-    (is [2 3 5] (p/primes 6))
-    (is [2 3 5 7] (p/primes 7))
-    (is [2 3 5 7] (p/primes 9))
-    (is [2 3 5 7] (p/primes 8))
-    (is [2 3 5 7] (p/primes 10))
-    (is [2 3 5 7 11 13 17 19 23 29] (p/primes 30))))
-
-(deftest prime?-test
-  (are [x y] (= x y)
-    true (p/prime? 2)
-  ))
+    (are [x y] (= x (p/primes y))
+      [2] 2
+      [2 3] 3
+      [2 3] 4
+      [2 3 5] 5
+      [2 3 5] 6
+      [2 3 5 7] 7
+      [2 3 5 7] 9
+      [2 3 5 7] 8
+      [2 3 5 7] 10
+      [2 3 5 7 11 13 17 19 23 29] 30)))
 
 
+(deftest categorize-test
+  (testing "small numbers"
+    (are [x y] (= x ((juxt p/unit? p/prime? p/composite?) y))
+      ;; unit? prime? composite? 
+      [true false false] 1
+      [false true false] 2
+      [false true false] 3
+      [false false true] 4
+      [false true false] 5
+      [false false true] 6
+      [false true false] 7
+      [false false true] 8
+      [false false true] 9
+      [false false true] 10)))
 
 (deftest integer>factors-map-test
   (testing "Negative numbers"
@@ -35,26 +43,27 @@
   (testing "Unit"
     (is (= {} (p/integer->factors-map 1))))
   (testing "Positive numbers"
-    (are [x y] (= x y)
-      {2 1} (p/integer->factors-map 2)
-      {3 1} (p/integer->factors-map 3)
-      {2 2} (p/integer->factors-map 4)
-      {5 1} (p/integer->factors-map 5)
-      {2 1, 3 1} (p/integer->factors-map 6)
-      {7 1} (p/integer->factors-map 7)
-      {2 3} (p/integer->factors-map 8)
-      {3 2} (p/integer->factors-map 9)
-      {2 1, 5 1} (p/integer->factors-map 10)
-      {11 1} (p/integer->factors-map 11)
-      {2 2, 3 1} (p/integer->factors-map 12)
-      {13 1} (p/integer->factors-map 13)
-      {2 1, 7 1} (p/integer->factors-map 14)
-      {3 1, 5 1} (p/integer->factors-map 15)
-      {2 4} (p/integer->factors-map 16)
-      {17 1} (p/integer->factors-map 17)
-      {2 1, 3 2} (p/integer->factors-map 18)
-      {19 1} (p/integer->factors-map 19)
-      {2 2, 5 1} (p/integer->factors-map 20))))
+    (are [x y] (= x (p/integer->factors-map y))
+      {} 1 
+      {2 1} 2
+      {3 1} 3
+      {2 2} 4
+      {5 1} 5
+      {2 1, 3 1} 6
+      {7 1} 7
+      {2 3} 8
+      {3 2} 9
+      {2 1, 5 1} 10
+      {11 1} 11
+      {2 2, 3 1} 12
+      {13 1} 13
+      {2 1, 7 1} 14
+      {3 1, 5 1} 15
+      {2 4} 16
+      {17 1} 17
+      {2 1, 3 2} 18
+      {19 1} 19
+      {2 2, 5 1} 20)))
 
 (deftest integer>factors-count-test
   (testing "Negative numbers"
@@ -112,7 +121,6 @@
       [[19]] (p/integer->factors-partitions 19)
       [[2 2] [5]] (p/integer->factors-partitions 20))))
 
-
 (deftest integer>factors-test
   (testing "Negative numbers"
     (is (thrown? Exception (p/integer->factors 0)))
@@ -140,7 +148,6 @@
       [2 3 3] (p/integer->factors 18)
       [19] (p/integer->factors 19)
       [2 2 5] (p/integer->factors 20))))
-
 
 (deftest factorization-properties-test
   (doseq [n (range 1 100)]
