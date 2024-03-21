1269f82b68fa6bb0ce7a953f4fefb3c552aa6759
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2024-03-21 18:36:26 +0200

    Add order

--- src/vk/ntheory/roots.clj
+++ src/vk/ntheory/roots.clj
@@ -4,13 +4,19 @@
             [clojure.core :as c])
   )
 
-
-
-
-
 (defn order
-  "Find multiplicative order of given integer `a` in Z/Zn"
+  "Find multiplicative order of given integer `a` in Z/Zn - {0}"
   [a n]
-  
+  (v/check-int-non-neg a)
+  (v/check-int-pos n)
+  (loop [k 1
+         an (mod a n)]
+    (condp = an
+      0 (throw (Exception. "Expected a and n relatively prime."))
+      1 k
+      (recur (inc k) (mod (* an a) n)))
+    )
   )
+
+(order 2 11)
 
