1d2dd9fc347c639e84b58d1976aa2ed6868f8f10
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2024-03-21 17:44:20 +0200

    Add roots

---
+++ src/vk/ntheory/roots.clj
@@ -0,0 +1,16 @@
+(ns vk.ntheory.roots
+  (:require [vk.ntheory.basic :as b]
+            [vk.ntheory.validation :as v]
+            [clojure.core :as c])
+  )
+
+
+
+
+
+(defn order
+  "Find multiplicative order of given integer `a` in Z/Zn"
+  [a n]
+  
+  )
+
