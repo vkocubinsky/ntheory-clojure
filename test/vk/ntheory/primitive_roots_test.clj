c20a64eed7b536cf1e66f9119731eeac933553f2
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2024-04-01 17:48:57 +0300

    s

--- test/vk/ntheory/primitive_roots_test.clj
+++ test/vk/ntheory/primitive_roots_test.clj
@@ -3,6 +3,11 @@
             [vk.ntheory.primitive-roots :as pr]))
 
 
+(deftest reduced-residue-test
+  (is #{1 2 3 4} (pr/reduced-residues 5))
+  (is #{1 2 4 5 7 8} (pr/reduced-residues 9))
+  (is #{1 5} (pr/reduced-residues 6)))
+
 (deftest order-test
   (are [x y] (= x y)
     10 (pr/order 2 11))
@@ -38,5 +43,16 @@
     18 #{5 11}
     19 #{2 3 10 13 14 15}
     20 #{}
+    21 #{}
+    22 #{7 13 17 19}
+    23 #{5 7 10 11 14 15 17 19 20 21}
+    24 #{}
+    25 #{2 3 8 12 13 17 22 23}
+    26 #{7 11 15 19}
+    27 #{2 5 11 14 20 23}
+    28 #{}
+    29 #{2 3 8 10 11 14 15 18 19 21 26 27}
+    30 #{}
+    31 #{3 11 12 13 17 21 22 24}
     )
   )
