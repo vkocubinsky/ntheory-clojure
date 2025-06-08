0a2fcdf20da2689de762095baddeb5c2862eaaa6
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2025-06-08 11:30:24 +0300

    Refactor

--- src/vk/ntheory/least_divisor_table.clj
+++ src/vk/ntheory/least_divisor_table.clj
@@ -1,57 +1,106 @@
+;; Least divisor table namespace.
+;;
+;; Least divisor table is an java array where element at index idx
+;; equal to least(prime) divisor of idx. Elements with index 0 and 1
+;; doesn't used.
+
 (ns vk.ntheory.least-divisor-table
   (:require [clojure.pprint :as pp]))
 
-(defn- table-find-next-prime
-  "Find next prime in least divisor table.
+(defn- find-prime
+  "Find prime in least divisor table.
+  
   Parameters: 
-    xs    - least divisor table
-    start - start index
-    end   - end index
+  - table: Least divisor table.
+  - start: Start index inclusive.
+  
+  Returns:
+  - first prime starts from given index, that is value in array for which index are equals to value.
   "
-  ([xs start] (table-find-next-prime xs start (count xs)))
-  ([^ints xs start end]
-   (when (< start end)
-     (let [e (aget xs start)]
-       (if (and (> start 1) (= e start))
-         start
-         (recur xs (inc start) end))))))
+  [table start]
+  (let [end (count table)]
+    (loop [idx start]
+      (when (< idx end)
+        (let [val (aget table idx)]
+          (if (= val idx)
+            idx
+            (recur (inc idx))))))))
 
-(defn- table-update
-  "Update least divisor table to new value if it was not set."
-  [^ints xs ^Integer k ^Integer v]
-  (let [e (aget xs k)]
-    (when-not (< e k)
-      (aset xs k v))))
+(defn- mark-multiple
+  "Mark idx in least divisor table as multiple of val if it is not already marked.
 
-(defn- make-table
-  "Build least divisor table.
-  Use slightly modified Eratosthenes algorithm for build least divisor table."
+  Parameters:
+  - table: Least divisor table
+  - idx: index
+  - val: multiple of
+
+  Return
+  "
+  [^ints table ^Integer idx ^Integer val]
+  (let [curr-val (aget table idx)]
+    (when (= curr-val idx)
+      (aset table idx val))))
+
+(defn make-table
+  "Make least divisor table.
+  
+  Use slightly modified Eratosthenes algorithm for build least divisor table.
+
+  Parameters:
+  - n: upper limit inclusive
+  "
   [n]
-  (loop [xs (int-array (range (inc n)))
-         p (table-find-next-prime xs 2)]
+  (loop [table (int-array (range (inc n)))
+         p  (find-prime table 2)]
     (if (or (nil? p) (> (* p p) n))
-      xs
+      table
       (do
         (doseq [k (range (* p p) (inc n) p)]
-          (table-update xs k p))
-        (recur xs (table-find-next-prime xs (inc p)))))))
+          (mark-multiple table k p))
+        (recur table (find-prime table (inc p)))))))
 
-(defn- int->factors
-  [^ints xs ^Integer n]
+(defn upper-limit
+  "Upper limit for given table"
+  [table]
+  (let [len (count table)]
+    (if (> len 0)
+      (dec len)
+      0)))
+
+(defn int->factors
+  "Factorize integer.
+
+  Returns: ordered factors of given integer
+  "
+  [^ints table ^Integer n]
+  (when-not (and (<= n (upper-limit table)) (> n 0))
+    (throw (ex-info "Bad argument" {:upper-limit (upper-limit table) :value n})))
   (lazy-seq
    (when (> n 1)
-     (let [d (aget xs n)]
-       (cons d (int->factors' xs (quot n d)))))))
+     (let [d (aget table n)]
+       (cons d (int->factors table (quot n d)))))))
 
-;; (make-table 10)
+(defn prime?
+  "Check does given integer is a prime."
+  [table n]
+  (when-not (and (<= n (upper-limit table)) (> n 0))
+    (throw (ex-info "Bad argument" {:upper-limit (upper-limit table) :value n})))
+  (let [val (aget table n)]
+    (= val n)))
 
-(defn table-print [xs]
-  (->> xs
+(defn table->primes
+  "Make primes vector from least divisor table"
+  [table]
+  (->> table
+       (keep-indexed #(when (= %1 %2) %1))
+       (drop-while #(< % 2))))
+
+(defn print-table [table]
+  (->> table
        (map-indexed (fn [idx val] {:index idx :value val}))
        (pp/print-table)))
 
-(-> 10
-    make-table
-    table-print)
+;; (-> 10 make-table print-table)
+;; (int->factors (make-table 100) 100)
 
-(int->factors (make-table 100) 100)
+
---
+++ test/vk/ntheory/least_divisor_table_test.clj
@@ -0,0 +1,210 @@
+(ns vk.ntheory.least-divisor-table-test
+  (:require
+   [clojure.test :refer [deftest is are testing]]
+   [vk.ntheory.least-divisor-table :as t]))
+
+
+
+
+(deftest check-prime-test
+  (testing "Primes"
+    (are [x] (= x (p/check-prime x))
+      3 5 7 11 13 19
+      ))
+  (testing "Not primes"
+    (are [x] (thrown? IllegalArgumentException (p/check-prime x))
+      -3 -2 -1 0 1))
+  (testing "Not integers"
+    (are [x] (thrown? IllegalArgumentException (p/check-prime x))
+      1.1 "s")))
+
+(deftest check-odd-prime-test
+  (testing "Odd primes"
+    (are [x] (= x (p/check-odd-prime x))
+      3 5 7 11 13 19
+      ))
+  (testing "Not odd primes"
+    (are [x] (thrown? IllegalArgumentException (p/check-odd-prime x))
+      -3 -2 -1 0 1 2))
+  (testing "Not integers"
+    (are [x] (thrown? IllegalArgumentException (p/check-odd-prime x))
+      1.1 "s")))
+
+
+(deftest primes-test
+  (testing "cache"
+    (p/cache-reset!)
+    (is (= 0 (:upper @p/cache))))
+  (testing "small numbers"
+    (are [x y] (= (p/primes x) y)
+      1 []
+      2 [2]
+      3 [2 3]
+      4 [2 3]
+      5 [2 3 5]
+      6 [2 3 5]
+      7 [2 3 5 7]
+      8 [2 3 5 7]
+      9 [2 3 5 7]
+      10 [2 3 5 7]
+      11 [2 3 5 7 11]
+      12 [2 3 5 7 11]
+      13 [2 3 5 7 11 13]
+      30 [2 3 5 7 11 13 17 19 23 29])))
+
+(deftest categorize-test
+  (testing "small numbers"
+    (are [x y] (= ((juxt p/unit? p/prime? p/composite?) x) y)
+      ;; number [unit? prime? composite?] 
+      1 [true false false]
+      2 [false true false]
+      3 [false true false]
+      4 [false false true]
+      5 [false true false]
+      6 [false false true]
+      7 [false true false]
+      8 [false false true]
+      9 [false false true]
+      10 [false false true])))
+
+(deftest int->factors-map-test
+  (testing "Negative numbers"
+    (is (thrown? Exception (p/int->factors-map 0)))
+    (is (thrown? Exception (p/int->factors-map -1))))
+  (testing "Positive numbers"
+    (are [x y] (= (p/int->factors-map x) y)
+      1  {}
+      2  {2 1}
+      3  {3 1}
+      4  {2 2}
+      5  {5 1}
+      6  {2 1, 3 1}
+      7  {7 1}
+      8  {2 3}
+      9  {3 2}
+      10 {2 1, 5 1}
+      11 {11 1}
+      12 {2 2, 3 1}
+      13 {13 1}
+      14 {2 1, 7 1}
+      15 {3 1, 5 1}
+      16 {2 4}
+      17 {17 1}
+      18 {2 1, 3 2}
+      19 {19 1}
+      20 {2 2, 5 1})))
+
+(deftest int->factors-count-test
+  (testing "Negative numbers"
+    (is (thrown? Exception (p/int->factors-count 0)))
+    (is (thrown? Exception (p/int->factors-count -1))))
+  (testing "Positive numbers"
+    (are [x y] (= (p/int->factors-count x) y)
+      1  []
+      2  [[2 1]]
+      3  [[3 1]]
+      4  [[2 2]]
+      5  [[5 1]]
+      6  [[2 1] [3 1]]
+      7  [[7 1]]
+      8  [[2 3]]
+      9  [[3 2]]
+      10 [[2 1] [5 1]]
+      11 [[11 1]]
+      12 [[2 2] [3 1]]
+      13 [[13 1]]
+      14 [[2 1] [7 1]]
+      15 [[3 1] [5 1]]
+      16 [[2 4]]
+      17 [[17 1]]
+      18 [[2 1] [3 2]]
+      19 [[19 1]]
+      20 [[2 2] [5 1]])))
+
+(deftest int->coprime-factors-test
+  (testing "Negative numbers"
+    (is (thrown? Exception (p/int->coprime-factors 0)))
+    (is (thrown? Exception (p/int->coprime-factors -1))))
+  (testing "Positive numbers"
+    (are [x y] (= (p/int->coprime-factors x) y)
+      1  []
+      2  [2]
+      3  [3]
+      4  [4]
+      5  [5]
+      6  [2 3]
+      7  [7]
+      8  [8]
+      9  [9]
+      10 [2 5]
+      11 [11]
+      12 [4 3]
+      13 [13]
+      14 [2 7]
+      15 [3 5]
+      16 [16]
+      17 [17]
+      18 [2 9]
+      19 [19]
+      20 [4 5])))
+
+(deftest int->factors-paritions-test
+  (testing "Negative numbers"
+    (is (thrown? Exception (p/int->factors-partitions 0)))
+    (is (thrown? Exception (p/int->factors-partitions -1))))
+  (testing "Positive numbers"
+    (are [x y] (= (p/int->factors-partitions x) y)
+      1  []
+      2  [[2]]
+      3  [[3]]
+      4  [[2 2]]
+      5  [[5]]
+      6  [[2] [3]]
+      7  [[7]]
+      8  [[2 2 2]]
+      9  [[3 3]]
+      10 [[2] [5]]
+      11 [[11]]
+      12 [[2 2] [3]]
+      13 [[13]]
+      14 [[2] [7]]
+      15 [[3] [5]]
+      16 [[2 2 2 2]]
+      17 [[17]]
+      18 [[2] [3 3]]
+      19 [[19]]
+      20 [[2 2] [5]])))
+(deftest int->factors-test
+  (testing "Negative numbers"
+    (is (thrown? Exception (p/int->factors 0)))
+    (is (thrown? Exception (p/int->factors -1))))
+  (testing "Positive numbers"
+    (are [x y] (= (p/int->factors x) y)
+      1  []
+      2  [2]
+      3  [3]
+      4  [2 2]
+      5  [5]
+      6  [2 3]
+      7  [7]
+      8  [2 2 2]
+      9  [3 3]
+      10 [2 5]
+      11 [11]
+      12 [2 2 3]
+      13 [13]
+      14 [2 7]
+      15 [3 5]
+      16 [2 2 2 2]
+      17 [17]
+      18 [2 3 3]
+      19 [19]
+      20 [2 2 5])))
+
+(deftest factorization-properties-test
+  (doseq [n (range 1 100)]
+    (is (= n (p/factors-count->int (p/int->factors-count n))))
+    (is (= n (p/factors-count->int (p/int->factors-map n))))
+    (is (= n (p/factors-partitions->int (p/int->factors-partitions n))))
+    (is (= n (p/factors->int (p/int->factors n))))
+    (is (= n (p/factors->int (p/int->coprime-factors n))))))
