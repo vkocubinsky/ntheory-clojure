536b1d047f296bc42a7fad19868a855054936226
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2025-08-17 10:26:13 +0300

    update multi table factorization

--- src/vk/ntheory/ldt/multi_table_factorization.clj
+++ src/vk/ntheory/ldt/multi_table_factorization.clj
@@ -7,20 +7,20 @@
             [vk.ntheory.ldt.multi-sieve :as s]
             [vk.ntheory.factorization :as f]))
 
-(defrecord FullTableFactorization [table]
+(defrecord FullTableFactorization [multi-table]
   f/Factorization
-  (factors [this n] (s/table-factors table n))
+  (factors [this n] (s/table-factors multi-table n))
   (factor-counts [this n] (f/factors->counts (f/factors this n)))
   (distinct-factors [this n] (f/factors->distinct (f/factors this n)))
-  (prime? [this n] (s/table-prime? table n))
-  (primes [this] (s/table-primes table))
-  (in-domain? [this n] (t/table-contains? table n)))
+  (prime? [this n] (s/table-prime? multi-table n))
+  (primes [this] (s/table-primes multi-table))
+  (in-domain? [this n] (t/table-contains? multi-table n)))
 
-(defrecord OddTableFactorization [table upper-limit]
+(defrecord OddTableFactorization [multi-table upper-limit]
   f/Factorization
   (factors [this n] (u/check-pos-int n)
     (let [[power-of-two rest] (u/power-of-two-parts n)]
-      (concat (repeat power-of-two 2) (s/table-factors table rest))))
+      (concat (repeat power-of-two 2) (s/table-factors multi-table rest))))
   (factor-counts [this n] (f/factors->counts (f/factors this n)))
   (distinct-factors [this n] (f/factors->distinct (f/factors this n)))
   (prime? [this n] (u/check-pos-int n)
@@ -28,17 +28,19 @@
       (< upper-limit 2) false
       (= n 2) true
       :else (let [[power-of-two rest] (u/power-of-two-parts n)]
-              (and (zero? power-of-two) (s/table-prime? table rest)))))
-  (primes [this] (let [seq (s/table-primes table)]
+              (and (zero? power-of-two) (s/table-prime? multi-table rest)))))
+  (primes [this] (let [seq (s/table-primes multi-table)]
                    (if (>= upper-limit 2)
                      (cons 2 seq)
                      seq)))
   (in-domain? [this n] (u/check-pos-int n)
     (let [[power-of-two rest] (u/power-of-two-parts n)]
-      (t/table-contains? table rest))))
+      (t/table-contains? multi-table rest))))
 
+;; Valery move this to share multi-table and table
 (defmulti make-factorization (fn [table-type upper-limit] table-type))
 
+;; Valery make multi table here instead of table
 (defmethod make-factorization :multi-full [_ upper-limit]
   (when-not (pos-int? upper-limit)
     (throw (ex-info "Upper limit must be positive integer" {:upper-limit upper-limit})))
@@ -49,7 +51,7 @@
     (s/sieve table 2)
     (->FullTableFactorization table)))
 
-(defmethod make-factorization :odd [_ upper-limit]
+(defmethod make-factorization :multi-odd [_ upper-limit]
   (when-not (pos-int? upper-limit)
     (throw (ex-info "Upper limit must be positive integer" {:upper-limit upper-limit})))
   (let [odd-upper-limit (if (odd? upper-limit)
