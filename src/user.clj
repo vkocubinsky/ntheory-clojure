73c010535bd174dd4aec622098433435e4d8baab
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2025-10-16 15:36:37 +0300

    store from machine valery2014.local at Thu Oct 16 15:36:37 EEST 2025

--- src/user.clj
+++ src/user.clj
@@ -1,6 +1,29 @@
 (ns user)
 
 (set! *warn-on-reflection* true)
+
+;; Load Compliment
+(try
+  (require 'compliment.core)
+  (println "Loaded compliment" )
+  (catch Exception e (println "Can't load compliment"))
+  )
+
+;; Load clj-reload
+(try
+  (require '[clj-reload.core :as reload])
+  (println "Loaded clj-reload" )
+  (catch Exception e (println "Can't load clj-reload"))
+)
+
+;; Load tools.namespace
+#_
+(try
+  (require '[clojure.tools.namespace.repl :refer [refresh]])
+  (println "Loaded tools.namespace" )
+  (catch Exception e (println "Can't load tools.namespace"))
+)
+
 (println "Hello from user namespace.")
 
 
