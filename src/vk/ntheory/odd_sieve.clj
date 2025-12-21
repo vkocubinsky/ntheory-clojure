57d42f5ae652e009c81bf0efd97ec7f8f8c4cc9a
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2025-12-21 17:36:21 +0200

    store from machine valery2014 at Sun Dec 21 17:36:21 EET 2025

--- src/vk/ntheory/odd_sieve.clj
+++ src/vk/ntheory/odd_sieve.clj
@@ -4,22 +4,19 @@
             [vk.ntheory.odd-table :as t]
             [vk.ntheory.factorization :as f]))
 
-
 (defn sieve
   "Sieve of Erathosphene."
-  [table start]
-  (loop [p  start]
-    (if (> (* p p) (t/table-upper-limit table))
+  [table]
+  (loop [p 3]
+    (if (> (* p p) (:upper-limit table))
       table
-      (let [p' (t/table-get-number table p)
-            mark-step (if (= p 2) p (* p 2))
-            iter-step (if (= p 2) 1 2)]
+      (let [p' (t/table-get-number table p)]
         (when (= p' p)
-          (doseq [k (range (* p p) (inc (t/table-upper-limit table)) mark-step)]
+          (doseq [k (range (* p p) (inc (:upper-limit table)) (* p 2))]
             (let [k' (t/table-get-number table k)]
               (when (= k' k)
                 (t/table-set-number! table k p)))))
-        (recur (+ p iter-step))))))
+        (recur (+ p 2))))))
 
 (defn table-factors
   "Factorize integer."
@@ -45,34 +42,29 @@
        (map first)
        (drop-while #(< % 2))))
 
-
-
 ;; end of sieve
-
-
 
 (defrecord OddTableFactorization [table upper-limit]
   f/Factorization
   (factors [this n] (u/check-pos-int n)
     (let [[power-of-two rest] (u/power-of-two-parts n)]
-      (concat (repeat power-of-two 2) (s/table-factors table rest))))
+      (concat (repeat power-of-two 2) (table-factors table rest))))
   (factor-counts [this n] (f/factors->counts (f/factors this n)))
   (distinct-factors [this n] (f/factors->distinct (f/factors this n)))
-  (prime? [this n] (u/check-pos-int n)
+  (prime? [this n]
+    (u/check-pos-int n)
     (cond
       (< upper-limit 2) false
       (= n 2) true
       :else (let [[power-of-two rest] (u/power-of-two-parts n)]
-              (and (zero? power-of-two) (s/table-prime? table rest)))))
-  (primes [this] (let [seq (s/table-primes table)]
+              (and (zero? power-of-two) (table-prime? table rest)))))
+  (primes [this] (let [seq (table-primes table)]
                    (if (>= upper-limit 2)
                      (cons 2 seq)
                      seq)))
   (in-domain? [this n] (u/check-pos-int n)
     (let [[power-of-two rest] (u/power-of-two-parts n)]
       (t/table-contains? table rest))))
-
-
 
 (defmethod f/make-factorization :odd-table [{:keys [upper-limit]}]
   (when-not (pos-int? upper-limit)
@@ -81,11 +73,10 @@
                           upper-limit
                           (dec upper-limit))
         table (t/make-table odd-upper-limit)]
-    (s/sieve table 3)
+    (sieve table)
     (->OddTableFactorization table upper-limit)))
 
 
-(comment )
 
 
 
--- test/vk/ntheory/factorization_test.clj
+++ test/vk/ntheory/factorization_test.clj
@@ -1,7 +1,10 @@
-(ns vk.ntheory.factorization-test
-  (:require [vk.ntheory.factorization :as f]
-            [clojure.test :refer [deftest is are testing]])
-  )
+(ns vk.ntheory.ldt.factorization-test
+  (:require
+   [clojure.test :refer [deftest is are testing]]
+   [vk.ntheory.factorization :as f]
+   [vk.ntheory.odd-sieve] ;; load implementation
+   [clojure.string :as str]))
+
 
 (deftest factors->distinct-test
   (is (= [2 3] (f/factors->distinct [2 2 3 3 3])))
@@ -17,4 +20,174 @@
 (deftest counts->factors-test
   (is (= [2 2 2 3 3] (f/counts->factors [[2 3] [3 2]])))
   )
+
+
+
+(def test-prop-upper-limit 30)
+
+(def factorizer-specs [{:type :odd-table
+                        :upper-limit test-prop-upper-limit}
+                       ])
+
+(defn run-for-all-factorizers [f]
+  (doseq [factorizer-spec factorizer-specs]
+    (testing (str "factorizer " factorizer-spec)
+      (let [factorizer (f/make-factorization factorizer-spec)]
+        (f factorizer)))))
+
+(defn run-for-all-numbers [f]
+  (letfn [(test-helper [factorizer]
+            (doseq [n (range 1 test-prop-upper-limit)]
+              (testing (str "number " n)
+                (f factorizer n))))]
+    (run-for-all-factorizers test-helper)))
+
+(deftest factors-test
+  (letfn [(test-helper [factorizer]
+            (testing "Positive numbers"
+              (are [x y] (= y (f/factors factorizer x))
+                1  []
+                2  [2]
+                3  [3]
+                4  [2 2]
+                5  [5]
+                6  [2 3]
+                7  [7]
+                8  [2 2 2]
+                9  [3 3]
+                10 [2 5]
+                11 [11]
+                12 [2 2 3]
+                13 [13]
+                14 [2 7]
+                15 [3 5]
+                16 [2 2 2 2]
+                17 [17]
+                18 [2 3 3]
+                19 [19]
+                20 [2 2 5])))]
+    (run-for-all-factorizers test-helper)))
+
+(deftest factors-prop-test
+  (letfn [(test-helper [factorizer n]
+            (let [factors (f/factors factorizer n)]
+              (is (= n (apply * factors)) "Product of factors n equals to n")
+              (is (= factors (sort factors)) "Factors must be ordered")))]
+    (run-for-all-numbers test-helper)))
+
+(deftest factor-counts-test
+  (letfn [(test-helper [factorizer]
+            (testing "Positive numbers"
+              (are [x y] (= y (f/factor-counts factorizer x))
+                1  []
+                2  [[2 1]]
+                3  [[3 1]]
+                4  [[2 2]]
+                5  [[5 1]]
+                6  [[2 1] [3 1]]
+                7  [[7 1]]
+                8  [[2 3]]
+                9  [[3 2]]
+                10 [[2 1] [5 1]]
+                11 [[11 1]]
+                12 [[2 2] [3 1]]
+                13 [[13 1]]
+                14 [[2 1] [7 1]]
+                15 [[3 1] [5 1]]
+                16 [[2 4]]
+                17 [[17 1]]
+                18 [[2 1] [3 2]]
+                19 [[19 1]]
+                20 [[2 2] [5 1]])))]
+    (run-for-all-factorizers test-helper)))
+
+(deftest factor-counts-prop-test
+  (letfn [(test-helper [factorizer n]
+            (let [counts (f/factor-counts factorizer n)
+                  factors (f/counts->factors counts)]
+              (is (= n (apply * factors)) "Product of factors n equals to n")
+              (is (= factors (sort factors)) "Factors must be ordered")))]
+    (run-for-all-numbers test-helper)))
+
+(deftest distinct-factors-test
+  (letfn [(test-helper [factorizer]
+            (testing "Positive numbers"
+              (are [x y] (= y (f/distinct-factors factorizer x))
+                1  []
+                2  [2]
+                3  [3]
+                4  [2]
+                5  [5]
+                6  [2 3]
+                7  [7]
+                8  [2]
+                9  [3]
+                10 [2 5]
+                11 [11]
+                12 [2 3]
+                13 [13]
+                14 [2 7]
+                15 [3 5]
+                16 [2]
+                17 [17]
+                18 [2 3]
+                19 [19]
+                20 [2 5])))]
+    (run-for-all-factorizers test-helper)))
+
+(deftest prime?-test
+  (letfn [(test-helper [factorizer]
+            (testing "Positive numbers"
+              (are [x y] (= (f/prime? factorizer x) y)
+                1  false
+                2  true
+                3  true
+                4  false
+                5  true
+                6  false
+                7  true
+                8  false
+                9  false
+                10 false
+                11 true
+                12 false
+                13 true
+                14 false
+                15 false
+                16 false
+                17 true
+                18 false
+                19 true
+                20 false)))]
+    (run-for-all-factorizers test-helper)))
+
+(deftest primes-test
+  (letfn [(test-helper [factorizer]
+            (is [2 3 5 7 11 13 17 19] (take-while #(< % 20) (f/primes factorizer))))]
+    (run-for-all-factorizers test-helper)))
+
+(deftest primes-prop-test
+  (letfn [(test-helper [factorizer]
+            (is (every? #(f/prime? factorizer %) (f/primes factorizer))))]
+    (run-for-all-factorizers test-helper)))
+
+(deftest in-domain?-test
+  (letfn [(test-helper [factorizer n]
+            (is (f/in-domain? factorizer n)))]
+    (run-for-all-numbers test-helper)))
+
+(deftest odd-factorizer-test
+  (let [factorizer (f/make-factorization {:type :odd-table :upper-limit test-prop-upper-limit})]
+    (doseq [n (range 1 test-prop-upper-limit)
+            k (range 1 3)]
+      (testing (str "number: " n " power of 2: " k)
+        (let [n' (apply * n (repeat k 2))
+              factors' (f/factors factorizer n')]
+          (is (f/in-domain? factorizer n'))
+          (is (= (repeat k 2) (take k factors'))))))))
+
+
+
+
+
 
