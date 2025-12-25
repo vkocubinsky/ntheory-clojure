92f625481a6b9970f6acb6443aa2fe8d93f7ad97
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2025-12-25 16:13:24 +0200

    store from machine valery2014 at Thu Dec 25 16:13:24 EET 2025

--- src/vk/ntheory/util.clj
+++ src/vk/ntheory/util.clj
@@ -9,8 +9,33 @@
     [k r]))
 
 
+(defn power-of-two-parts'
+  "Returns [power-of-two, odd-part] for given integer (Long, BigInteger, or clojure.lang.BigInt)."
+  [n]
+  (cond
+    (instance? Long n)
+      (let [k (Long/numberOfTrailingZeros n)
+            r (bit-shift-right n k)]
+        [k r])
+    (instance? java.math.BigInteger n)
+      (let [k (.getLowestSetBit n)
+            r (.shiftRight n k)]
+        [k r])
+    (instance? clojure.lang.BigInt n)
+      (let [bi (.toBigInteger n)]
+        (let [k (.getLowestSetBit bi)
+              r (.shiftRight bi k)]
+          [k r]))
+    :else
+      (throw (IllegalArgumentException.
+              "n must be Long, BigInteger, or clojure.lang.BigInt"))))
+
+
+
 (comment
   (power-of-two-parts 7)
+
+  (power-of-two-parts' 8N)
 
   )
 
