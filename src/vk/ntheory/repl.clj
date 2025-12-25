da62afad6743a6f09485dbcd72651e84bc1c3afc
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2025-12-25 15:57:54 +0200

    store from machine valery2014 at Thu Dec 25 15:57:54 EET 2025

---
+++ src/vk/ntheory/repl.clj
@@ -0,0 +1,32 @@
+(ns vk.ntheory.repl
+  (:require
+   [vk.ntheory.even-wrapper :as sieve]
+   [vk.ntheory.factorization :as fz])
+  )
+
+;; todo: use atom
+(defonce fz (fz/make {:type :even-wrapper :cache-upper-limit 8192}))
+
+(defn primes [n]
+  (take n (fz/primes fz))
+  )
+
+(defn prime? [n]
+  (fz/prime? fz n)
+  )
+
+(defn factors [n]
+  (fz/factors fz n)
+  )
+
+(defn factor-counts [n]
+  (fz/factor-counts fz n)
+  )
+
+
+(defn distinct-factors [n]
+  (fz/distinct-factors fz n)
+  )
+
+
+
