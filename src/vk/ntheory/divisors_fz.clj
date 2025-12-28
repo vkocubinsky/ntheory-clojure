0a69e77ce468f2d23dd03fa6b2f83b313454fc11
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2025-12-28 14:53:47 +0200

    store from machine valery2014.local at Sun Dec 28 14:53:47 EET 2025

--- src/vk/ntheory/divisors_fz.clj
+++ src/vk/ntheory/divisors_fz.clj
@@ -1,32 +1,55 @@
 (ns vk.ntheory.divisors-fz
   (:require [vk.ntheory.even-fz :as even-fz]
-            [vk.ntheory.factorization :as fz])
-  )
+            [vk.ntheory.factorization :as fz]))
 
+(defn divides?
+  "Returns true if d divides n, otherwise false."
+  [d n]
+  (assert (and (pos? d) (pos? n)))
+  (zero? (mod n d)))
 
-(defrecord DivisorsFactorization [even-fz value partitions]
+(defn divisors-factors [n partitions]
+  (letfn [(shift [partitions]
+            (let [head (first partitions)
+                  head' (rest head)
+                  tail (rest partitions)]
+              (if (empty? head') tail (cons head' tail))))
+          (lazy-factors [r partitions]
+            (lazy-seq (cond
+                        (= r 1) ()
+                        (empty? partitions) (list r)
+                        (zero? (mod r (ffirst partitions))) (cons (ffirst partitions) (lazy-factors (quot r (ffirst partitions)) (shift partitions)))
+                        :else (lazy-factors r (rest partitions)))))]
+    (lazy-factors n partitions)))
+
+(defrecord DivisorsFactorization [parent-fz value partitions]
   fz/Factorization
-    (factors [this n]
+  (factors [this n]
     (assert (fz/in-domain? this n))
-    nil)
+    (if (divides? n value)
+      (divisors-factors n partitions)
+      (fz/factors parent-fz n)))
   (prime? [this n]
     (assert (fz/in-domain? this n))
-    nil)
-  (primes [this] nil)
+    (if (divides? n value)
+      (some? (some #(= n %) (map first partitions)))
+      (fz/prime? parent-fz n)))
+  (primes [this]
+    (fz/primes parent-fz))
+
   (in-domain? [this n]
-    (and (pos? value) (zero? (mod value n))))
-  )
+    (pos? value)))
 
-
-(defmethod fz/make :divisors-fz [{:keys [parent-fz value]} ]
+(defmethod fz/make :divisors-fz [{:keys [parent-fz value]}]
   (assert (pos? value))
   (let [partitions (fz/factor-partitions parent-fz value)])
-  (-> DivisorsFactorization parent-fz (fz/factor-partitions value))
-  )
+  (->DivisorsFactorization parent-fz value (fz/factor-partitions parent-fz value)))
 
 (comment
+  (divisors-in-domain? 98 49)
 
   (let [even-fz (fz/make {:type :even-fz :cache-upper-limit 100})
-        fz (fz/make {:type :divisors-fz :parent-fz even-fz})]
-    (fz/factors fz  98))
+        fz (fz/make {:type :divisors-fz :parent-fz even-fz :value 98})]
+    (fz/factors fz  49))
+
   )
