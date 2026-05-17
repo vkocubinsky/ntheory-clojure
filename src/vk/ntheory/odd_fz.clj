525c3a0cde8b9e0904d6e99f69e5d5b500112500
Valery Kocubinsky <valery.kocubinsky@gmail.com>
2026-05-17 10:45:00 +0300

    store from machine valery2017 at Sun May 17 10:45:00 EEST 2026

--- src/vk/ntheory/odd_fz.clj
+++ src/vk/ntheory/odd_fz.clj
@@ -12,18 +12,17 @@
   [{:keys [parent-fz parent-upper-limit] :as spec} start end]
   {:pre [(odd? start)]}
   (let [primes (fz/primes parent-fz)]
-     (cond
-       (not (<= start end)) ()
-       (<= parent-upper-limit start end)  (range start (inc end) 2)
-       (<= start parent-upper-limit end)  (concat (take-while #(<= start % parent-upper-limit) primes)
-                                                  (range (+ 2 parent-upper-limit) (inc end) 2))
-       (<= start end parent-upper-limit)  (take-while #(<= start % end) primes))))
+    (cond
+      (not (<= start end)) ()
+      (<= parent-upper-limit start end)  (range start (inc end) 2)
+      (<= start parent-upper-limit end)  (concat (take-while #(<= start % parent-upper-limit) primes)
+                                                 (range (+ 2 parent-upper-limit) (inc end) 2))
+      (<= start end parent-upper-limit)  (take-while #(<= start % end) primes))))
 
 ;; eager version
-(defn- odd-factors [{:keys [pseudo-prime? pseudo-certainty parent-upper-limit parent-fz] :as spec} n]
+(defn- odd-factors [{:keys [parent-upper-limit parent-fz] :as spec} n]
   (loop [r n
          factors []
-         r-changed? true
          trials (trials spec 3 (sqrt (+ n 0.5)))]
     (let [trial (first trials)]
       (cond
@@ -31,38 +30,30 @@
         (= r 1) factors
         (> (* trial trial) r) (conj factors r)
         (fz/in-domain? parent-fz r) (into factors (fz/factors parent-fz r))
-        (zero? (mod r trial)) (recur (quot r trial) (conj factors trial) true trials)
-        (and pseudo-prime? r-changed? (util/probable-prime r pseudo-certainty)) (conj factors r)
-        :else (recur r factors false (next trials))))))
-
-(defn- first-divisor
-  "First divisor of `n` starts with `start` inclusive"
-  [start n]
-  {:pre [(> start 1) (<= start n)]}
-  (first (filter #(zero? (mod n %)) (iterate #(+ % 2) start))))
+        (zero? (mod r trial)) (recur (quot r trial) (conj factors trial) trials)
+        :else (recur r factors (next trials))))))
 
 (defn- odd-prime? [{:keys [pseudo-prime? pseudo-certainty parent-fz parent-upper-limit]} n]
-  (cond
-    (fz/in-domain? parent-fz n) (fz/prime? parent-fz n)
-    pseudo-prime? (util/probable-prime n pseudo-certainty)
-    :else (= n (first-divisor (+ parent-upper-limit 2) n))))
+  (letfn [(first-divisor [start n]
+            (first (filter #(zero? (mod n %)) (range start (inc n) 2))))]
+    (cond
+      (fz/in-domain? parent-fz n) (fz/prime? parent-fz n)
+      pseudo-prime? (util/probable-prime n pseudo-certainty)
+      :else (= n (first-divisor (+ 2 parent-upper-limit) n)))))
 
 (defn- odd-primes [{:keys [parent-upper-limit parent-fz] :as spec}]
-  (concat (fz/primes parent-fz) (filter #(odd-prime? spec %) (iterate #(+ % 2) (+ 2 parent-upper-limit)))))
+  (concat (fz/primes parent-fz) (filter #(odd-prime? spec %) (iterate #(+ 2 %) (+ 2 parent-upper-limit)))))
 
-(defn- next-prime-no-parent
-  "Next prime after `n`"
-  [{:keys [pseudo-prime?] :as spec} n]
-  (if pseudo-prime?
-    (util/next-probable-prime n)
-    (first (filter #(odd-prime? spec %) (iterate #(+ % 2) (+ 2 n))))))
-
-(defn- odd-next-prime [{:keys [parent-fz] :as spec} n]
-  (if (fz/in-domain? parent-fz n)
-    (if-let [p (fz/next-prime parent-fz n)]
-      p
-      (next-prime-no-parent spec n))
-    (next-prime-no-parent spec n)))
+(defn- odd-next-prime [{:keys [parent-fz pseudo-prime?] :as spec} n]
+  (letfn [(next-prime' [n]
+            (if pseudo-prime?
+              (util/next-probable-prime n)
+              (first (filter #(odd-prime? spec %) (iterate #(+ 2 %) (+ 2 n))))))]
+    (if (fz/in-domain? parent-fz n)
+      (if-let [p (fz/next-prime parent-fz n)]
+        p
+        (next-prime' n))
+      (next-prime' n))))
 
 (defrecord OddFactorization [spec]
   fz/Factorization
@@ -101,14 +92,18 @@
 (comment
 
   (time
-   (let [parent-fz (fz/make {:type :odd-sieve-fz :upper-limit 1})
-         fz (fz/make {:type :odd-fz :pseudo-prime? true :pseudo-certainty 100 :parent-fz parent-fz})]
-     (doseq [x (range 1 100000 2)]
+   (let [parent-fz (fz/make {:type :odd-sieve-fz :upper-limit 100001})
+         fz (fz/make {:type :odd-fz :pseudo-prime? false :pseudo-certainty 100 :parent-fz parent-fz})]
+     (doseq [x (range 1 1000000 2)]
        (fz/factors fz x))))
 
   (let [parent-fz (fz/make {:type :odd-sieve-fz :upper-limit 1})
         fz (fz/make {:type :odd-fz :pseudo-prime? false :pseudo-certainty 100 :parent-fz parent-fz})]
-    (fz/factors fz 77))
+    (println "factors:" (fz/factors fz 77))
+    (println "next prime:" (fz/next-prime fz 19))
+    (println "primes:" (take 10 (fz/primes fz)))
+    (println "next prime of 23:" (fz/next-prime fz 23))
+    )
 
   (let [fz (fz/make {:type :odd-fz :pseudo-prime? true :pseudo-certainty 100 :parent-fz {:type :odd-sieve-fz :upper-limit 11}})]
     (println (fz/factors fz 491111113331)))
